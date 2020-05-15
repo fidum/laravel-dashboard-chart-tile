6207df0b2cfbb1ea9b149a1d69a8a08f2ccd5073
dmason30 <danmason@spendology.com>
2020-05-15 14:03:28 +0100

    Add refreshIntervalInSeconds tile property

--- tests/Unit/Components/ChartRefreshComponentTest.php
+++ tests/Unit/Components/ChartRefreshComponentTest.php
@@ -3,6 +3,8 @@
 namespace Fidum\ChartTile\Tests\Unit\Components;
 
 use Fidum\ChartTile\Components\ChartRefreshComponent;
+use Fidum\ChartTile\Examples\ExampleBarChart;
+use Fidum\ChartTile\Examples\ExamplePieChart;
 use Fidum\ChartTile\Factories\DefaultChartFactory;
 use Fidum\ChartTile\Tests\TestCase;
 use Livewire\Livewire;
@@ -28,21 +30,45 @@ class ChartRefreshComponentTest extends TestCase
         /** @var TestableLivewire $result */
         $result = Livewire::test(ChartRefreshComponent::class)
             ->set('position', 'a1:a2')
+            ->call('render');
+
+        $html = $result->payload['dom'];
+        $wireId = $result->payload['id'];
+
+        $result->assertEmitted('chartDataRefreshed' . $wireId)
+            ->assertViewHas('chartFactory', DefaultChartFactory::class)
+            ->assertViewHas('refreshIntervalInSeconds', 300)
+            ->assertViewHas('wireId', $wireId)
+            ->assertViewHas('height', '100%')
+        ;
+
+        (new ViewAssertion($html))
+            ->contains('<div wire:id="'.$wireId.'" class="hidden" wire:poll.300s></div>')
+        ;
+    }
+
+    public function testRenderCustomProperties()
+    {
+        /** @var TestableLivewire $result */
+        $result = Livewire::test(ChartRefreshComponent::class)
+            ->set('chartFactory', ExamplePieChart::class)
+            ->set('refreshIntervalInSeconds', 60)
             ->set('wireId', 'abc')
+            ->set('height', '75vh')
             ->call('render');
 
         $html = $result->payload['dom'];
         $wireId = $result->payload['id'];
 
         $result->assertEmitted('chartDataRefreshedabc')
-            ->assertViewHas('chartFactory', DefaultChartFactory::class)
-            ->assertViewHas('refreshIntervalInSeconds', 300)
+            ->assertViewHas('chartFactory', ExamplePieChart::class)
+            ->assertViewHas('refreshIntervalInSeconds', 60)
             ->assertViewHas('wireId', 'abc')
-            ->assertViewHas('height', '100%')
+            ->assertViewHas('height', '75vh')
         ;
 
         (new ViewAssertion($html))
-            ->contains('<div wire:id="'.$wireId.'" class="hidden" wire:poll.300s></div>')
+            ->contains('<div wire:id="'.$wireId.'" class="hidden" wire:poll.60s></div>')
         ;
     }
 }
